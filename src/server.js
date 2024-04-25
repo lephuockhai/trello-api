d761a4decdaf56b1deeb4da112cd23ec0609a292
lephuockhai <lephuockhai2015@gmail.com>
2024-04-25 17:09:56 +0700

    Init Code base

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
+
 import express from 'express'
-// const express = require('express')
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
-const localhost = 'localhost'
-const port = 5343
+const hostname = 'localhost'
+const port = 8017
 
-app.get('/', function(req, res) {
-    res.send('<h1>Hello KhaxiDev</h1>')
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.send('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, localhost, () => {
-    console.log(`hi guy I'm running on host: http://${localhost}:${port}/`)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
 })
