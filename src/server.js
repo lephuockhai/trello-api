8e64c7e0924df62c18507359c58bffaa3e6dc3a0
lephuockhai <lephuockhai2015@gmail.com>
2024-04-28 21:28:10 +0700

    Connect Mongodb ENV Variavles

--- src/server.js
+++ src/server.js
@@ -1,27 +1,42 @@
-
+// eslint-disable-next-line no-console
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { COLSE_DB, CONNECT_DB, GET_DB } from '~/config/config.mongodb'
+import exitHook from 'async-exit-hook'
+import { env } from './config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = env.APP_HOST
+  const port = env.APP_PORT
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.send('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    res.send('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`3. Hello ${env.AUTHOR}, I am running at http://${ hostname }:${ port }/`)
+  })
+  //thực hiện các tác vụ cleanup trước khi dừng hẵn server
+  // thư viện thay thế cho process.on ...
+  exitHook(() => {
+    console.log('4. Disconnecting DB...')
+    COLSE_DB()
+    console.log('5. Disconnected DB...')
+  })
+}
+
+// IIFE structure
+(
+  async () => {
+    try {
+      console.log('1. Connecting to mongoDb Atlas!')
+      await CONNECT_DB()
+      console.log('2. Connected to mongoDb Atlas!')
+      START_SERVER()
+    } catch (error) {
+      console.error(error)
+      process.exit(0)
+    }
+  }
+)()
