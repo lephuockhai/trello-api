7eb4bd765120815743a091c7ae827ff0aac0497b
lephuockhai <lephuockhai2015@gmail.com>
2024-04-30 02:00:06 +0700

    Create Model & Create new Board to DB

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 //process logic after handling error on the back step
+import { boardModel } from '~/models/boardModel'
 import { slugify } from '~/utils/formaters'
 
 const createNew = async (reqBody) => {
@@ -10,12 +11,19 @@ const createNew = async (reqBody) => {
         }
 
         // gọi đến tầng model để xử lý dữ liệu và lưu newboard đến database
+        const createdBoard = await boardModel.createNew(newBoard)
+        console.log('createdBoard:::', createdBoard)
+
+        const getNewBoard = await boardModel.findById(createdBoard.insertedId)
+
+        console.log('getNewBoard::::', getNewBoard)
+
         // xử lý các collection khác tuỳ đặc thù dự án ...
         //bắn email notification về cho admin khi có 1 cái boảd mới được tạo
-        return newBoard
+        return getNewBoard
     } catch (error) {
         //nếu như bên service có lỗi thì nó sẽ trả lỗi về controller
-        throw error
+        throw error //error ở đây sẽ trả về cho controller nên không cần new
     }
 }
 
